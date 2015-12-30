f42164129971c221bdf395f35a2a47ad1b951f0d
rokofler <rokofler@8d848460-8aa4-42f5-9949-60773694d80f>
2015-12-30 13:26:01 +0000

    updated new feature min-valley ro

--- popte2core/src/corete/data/tesignature/Chunk2SignatureParser.java
+++ popte2core/src/corete/data/tesignature/Chunk2SignatureParser.java
@@ -25,7 +25,7 @@ public class Chunk2SignatureParser {
 	private final String chromosome;
 	private final ArrayList<String> teshortcuts;
 	private final TEFamilyShortcutTranslator translator;
-	private final ArrayList<HashMap<Integer,PpileupSampleSummary>> hsms;
+	private final ArrayList<SampleChunk2SignatureParser> sc2sps;
 	public Chunk2SignatureParser(PpileupChunk chunk,ArrayList<Integer> windowsizes, ArrayList<Integer> valleysizes, int mincount, TEFamilyShortcutTranslator translator)
 	{
 		this.toparse=chunk;
@@ -39,11 +39,11 @@ public class Chunk2SignatureParser {
 		this.translator=translator;
 
 
-		hsms=new ArrayList<HashMap<Integer,PpileupSampleSummary>>();
+		sc2sps=new ArrayList<SampleChunk2SignatureParser>();
 		for(int i=0; i<this.toparse.size(); i++)
 		{
 			HashMap<Integer,PpileupSampleSummary> psst= toparse.getPSSTrack(i);
-			hsms.add(psst);
+			sc2sps.add(new SampleChunk2SignatureParser(psst,this.chromosome,this.start,this.end,this.translator));
 		}
 
 	}
@@ -86,144 +86,17 @@ public class Chunk2SignatureParser {
 
 		// for all te shortcuts
 		for(String s:this.teshortcuts) {
-			ArrayList<SignatureRangeInfo> t=this.getRanges(s,popindex);
+			ArrayList<SignatureRangeInfo> t=this.sc2sps.get(popindex).getRanges(s,popindex,this.windowsizes.get(popindex),
+					this.valleysizes.get(popindex),this.mincount);
 			signatures.addAll(t);
 		}
 		return signatures;
 	}
 
 
-	/**
-	 * Get the range signatures for one specific TE family and one specific sample id (popindex)
-	 * @param teshortcut
-	 * @param popindex
-	 * @return
-	 */
-	public ArrayList<SignatureRangeInfo> getRanges(String teshortcut,int popindex)
-	{
-		return getTEspecificSampleSignatures(hsms.get(popindex),teshortcut,popindex,this.windowsizes.get(popindex),this.valleysizes.get(popindex));
-	}
-
-	/**
-	 * For one TE in a given sample
-	 * @param sample
-	 * @return
-	 */
-	private ArrayList<SignatureRangeInfo> getTEspecificSampleSignatures(HashMap<Integer,PpileupSampleSummary> sample, String teshortcut,int popindex,int windowsize,int valleysize)
-	{
-		ArrayList<SignatureRangeInfo> toret=new ArrayList<SignatureRangeInfo>();
-
-		LinkedList<ScoreHelper> window=new LinkedList<ScoreHelper>();
-
-		int lastMaxStart=-1;
-		int lastMaxPosition=-1;
-		double lastMaxSupport=0.0;
-		double runningsum=0.0;
-		int minstretchcount=0;
-
-		int rangeStart=-1;
-		int rangeEnd=-1;
-		double winStartScore=0.0;
-		double winEndScore=0.0;
-
-		for(int i=start; i<=end; i++)
-		{
-
-			int tecount=sample.get(i).getTEcount(teshortcut);
-			runningsum+=tecount;
-			window.add(new ScoreHelper(i,tecount));
-			if(window.size()>windowsize){
-				ScoreHelper pop=window.remove(0);
-				runningsum-=pop.score;
-			}
-			if(window.size()!=windowsize)continue;
-
-			double av=runningsum/(double)windowsize;
-			if(av>=this.mincount)
-			{
-				if(rangeStart==-1){
-					rangeStart=window.getFirst().position;
-				}
-				rangeEnd=window.getLast().position;
-
-				// larger than the mincount
-				   if(av>=lastMaxSupport)
-				   {
-					   // larger than the previous maximum count
-
-					   lastMaxStart=window.getFirst().position;
-					   lastMaxSupport=av;
-					   lastMaxPosition=i;
-					   winStartScore=window.getFirst().score;
-					   winEndScore=window.getLast().score;
-				   }
-				minstretchcount=0;
-
-			}
-			else
-			{
-				// lower than the mincount
-				minstretchcount++;
 
 
-				if(lastMaxPosition!=-1 && minstretchcount>=(valleysize+windowsize-1))
-				{   // we had a previous highscore => signify (create signature)
 
-
-					SignatureRangeInfo sri=signify(lastMaxStart,lastMaxPosition,lastMaxSupport,teshortcut,popindex,rangeStart,rangeEnd,winStartScore,winEndScore);
-					toret.add(sri);
-					lastMaxSupport=0.0;
-					lastMaxPosition=-1;
-					lastMaxStart=-1;
-				}
-				if(minstretchcount>=(valleysize+windowsize-1)){
-					rangeStart=-1;
-					rangeEnd=-1;
-				}
-
-
-			}
-
-
-		}
-		// also process the last  // if a last exists
-		if(lastMaxPosition!=-1) {
-
-
-			SignatureRangeInfo sri = signify(lastMaxStart, lastMaxPosition, lastMaxSupport,teshortcut,popindex,rangeStart,rangeEnd,winStartScore,winEndScore);
-			toret.add(sri);
-		}
-		return toret;
-
-	}
-
-	private SignatureRangeInfo signify(int start,int end,double support,String shortcut,int popindex,int rangeStart,int rangeEnd,double winStartScore,double winEndScore)
-	{
-
-		PopulationID popid=new PopulationID(new ArrayList<Integer>(Arrays.asList(popindex)));
-		// forward = uppercase is end
-		// reverse = lowercase is start
-		SignatureDirection sigDir=translator.getSignatureDirection(shortcut);
-		String tefamily=translator.getFamilyname(shortcut);
-
-
-		InsertionSignature sig= new InsertionSignature(popid,this.chromosome,sigDir,start,end,tefamily, TEStrand.Unknown);
-		SignatureRangeInfo sri=new SignatureRangeInfo(sig,rangeStart,rangeEnd,winStartScore,winEndScore);
-		return sri;
-
-	}
-
-
-	private class ScoreHelper{
-		public int position;
-		public double score;
-		ScoreHelper(int position, double score)
-		{
-			this.position=position;
-			this.score=score;
-		}
-
-	}
 
 
 
